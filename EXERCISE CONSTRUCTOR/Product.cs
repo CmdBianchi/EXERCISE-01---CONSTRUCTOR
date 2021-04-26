a8a2ad6594f61bf2eee026b163ba89a5b92ab682
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-26 16:43:47 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE CONSTRUCTOR/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace EXERCISE_CONSTRUCTOR{
+    //----------------------START | OBJ PRODUCT------------------------//
+    class Product {
+        public string Name;
+        public double Price;
+        public double Amount;
+    //------------------------ MET. CLASS -------------------------------//
+        public double TotalValueStock() {
+            return Price * Amount;
+        }
+        public void AddProduct(int amount) {
+            Amount += amount;
+        }
+        public void RemoveProduct(int amount) {
+            Amount -= amount;
+        }
+
+        public override string ToString() {
+            return Name
+            + "| $ " + Price.ToString("F2") +"| " + Amount + "  Unidades | Total: $ " + TotalValueStock().ToString("F2");
+        }
+
+    }
+    //--------------------------- END --------------------------------//  
+}
---
+++ EXERCISE CONSTRUCTOR/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace EXERCISE_CONSTRUCTOR {
+    class Program {
+        /// -------> START / MAIN 
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args) {
+            Product p = new Product();
+
+            Console.Write("Entre os dados do produto: ");
+            Console.Write("Nome: "); p.Name = Console.ReadLine();
+            Console.Write("Preço: "); p.Price = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: "); p.Amount = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Dados do produto: "+p);
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int amnt = int.Parse(Console.ReadLine());
+            p.AddProduct(amnt);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados att: "+p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque: ");
+            amnt = int.Parse(Console.ReadLine());
+            p.RemoveProduct(amnt);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados att: "+p);
+            Console.WriteLine();
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        /// -------> END
+    }
+}
